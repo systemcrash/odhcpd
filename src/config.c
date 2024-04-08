9ac10810bba807026a12731aa363f99e8435c8a3
Paul Donald <newtwen@gmail.com>
2024-04-08 23:49:28 +0200

    config: clamp dhcpv6_hostid_len
    Attempt to be helpful.
    
    Signed-off-by: Paul Donald <newtwen@gmail.com>

--- src/config.c
+++ src/config.c
@@ -896,11 +896,12 @@ int config_parse_interface(void *data, size_t len, const char *name, bool overwr
 	if ((c = tb[IFACE_ATTR_DHCPV6_HOSTID_LEN])) {
 		uint32_t hostid_len = blobmsg_get_u32(c);
 
-		if (hostid_len >= HOSTID_LEN_MIN && hostid_len <= HOSTID_LEN_MAX)
-			iface->dhcpv6_hostid_len = hostid_len;
-		else
-			syslog(LOG_ERR, "Invalid %s value configured for interface '%s'",
-				iface_attrs[IFACE_ATTR_DHCPV6_HOSTID_LEN].name, iface->name);
+		iface->dhcpv6_hostid_len = (hostid_len < HOSTID_LEN_MIN) ? 
+			HOSTID_LEN_MIN : (hostid_len > HOSTID_LEN_MAX) ? 
+				HOSTID_LEN_MAX : hostid_len;
+		if (!(hostid_len >= HOSTID_LEN_MIN && hostid_len <= HOSTID_LEN_MAX))
+			syslog(LOG_INFO, "Clamped invalid %s value configured for interface '%s' to %d",
+				iface_attrs[IFACE_ATTR_DHCPV6_HOSTID_LEN].name, iface->name, iface->dhcpv6_hostid_len);
 
 	}
 
